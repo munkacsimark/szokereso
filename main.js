98340273bfb0c9cc0e77f9bd17841924d4e6b16a
Munkácsi Márk <munkacsimark@gmail.com>
2014-10-23 00:35:26 +0200

    tabla kirajzolasa

---
+++ main.js
@@ -0,0 +1,36 @@
+function Szokereso(abc, words) {
+	var container = $('#game');
+
+	var writeRandomLetter = function(abc) {
+		var selectedLetter = abc[Math.floor(Math.random()*abc.length)];
+		return selectedLetter;
+	}
+
+	var drawTable = function(rows, columns) {
+		var temp = '<table>';
+		for(var i = 0; i <= rows; i++) {
+			temp += '<tr>';
+			for(var j = 0; j <= columns; j++) {
+				temp += '<td data-row="' + i + '" data-column="' + j + '">'+ writeRandomLetter(abc) +'</td>';
+			};
+			temp += '</tr>';
+		};
+		temp += '</table>'
+		container.append(temp);
+	}
+
+	if (container.length != 0) {
+		drawTable(10, 10);
+	} else {
+		return;
+	}
+}
+
+var hunAbc = ['A', 'Á', 'B', 'C', 'D', 'E', 'É', 'F', 'G', 'H', 'I', 'Í', 'J', 'K', 'L', 'M', 'N', 'O', 'Ó', 'Ö', 'Ő', 'P', 'Q', 'R', 'S', 'T', 'U', 'Ú', 'Ü', 'Ű', 'V', 'W', 'X', 'Y', 'Z'];
+var hunWords = ['KAKA', 'PISI', 'VALAMI'];
+
+$(document).ready(function() {
+
+	var szokereso = new Szokereso(hunAbc, hunWords);
+
+});
