49ce6d39d2100ecbfc2e090afa03578a6a727859
Munkácsi Márk <munkacsimark@gmail.com>
2014-10-23 13:35:28 +0200

    keresendo szo elhelyezve

--- main.js
+++ main.js
@@ -1,9 +1,42 @@
 function Szokereso(abc, words) {
 	var container = $('#game');
+	var dimensions = [10, 10];
 
-	var writeRandomLetter = function(abc) {
-		var selectedLetter = abc[Math.floor(Math.random()*abc.length)];
+	var getRandomElement = function(array) {
+		var selectedLetter = array[Math.floor(Math.random()*array.length)];
 		return selectedLetter;
+	}
+
+	var getStartPoint = function(rows, columns) {
+		var randomPoint = [];
+		randomPoint.push(Math.floor((Math.random() * rows) + 1));
+		randomPoint.push(Math.floor((Math.random() * columns) + 1));
+		return randomPoint;
+	}
+
+	var exsistInHistory = function(coordinate, historyArray) {
+		for (var i = 0; i < historyArray.length; i++) {
+			if (historyArray[i][0] == coordinate[0] && historyArray[i][1] == coordinate[1]) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	var getRandomDirection = function(activePosition, history) {
+		var upCoordinates = [activePosition[0] - 1, activePosition[1]];
+		var downCoordinates = [activePosition[0] + 1, activePosition[1]];
+		var rightCoordinates = [activePosition[0], activePosition[1] + 1];
+		var leftCoordinates = [activePosition[0], activePosition[1] - 1];
+		var newActivePosition = [];
+		do {
+			newActivePosition = getRandomElement([upCoordinates, downCoordinates, rightCoordinates, leftCoordinates]);
+		} while (
+			(newActivePosition[0] < 1 || newActivePosition[0] > dimensions[0]) ||
+			(newActivePosition[1] < 1 || newActivePosition[1] > dimensions[1]) ||
+			exsistInHistory(newActivePosition, history)
+		);
+		return newActivePosition;
 	}
 
 	var positionCenterTable = function() {
@@ -13,31 +46,52 @@ function Szokereso(abc, words) {
 			'left' : xPos + 'px',
 			'top' : yPos + 'px'
 		});
+		console.log('Tábla középre igazítva');
 	}
 
 	var drawTable = function(rows, columns) {
 		var temp = '<table>';
-		for(var i = 0; i <= rows; i++) {
+		for (var i = 1; i <= rows; i++) {
 			temp += '<tr>';
-			for(var j = 0; j <= columns; j++) {
-				temp += '<td data-row="' + i + '" data-column="' + j + '">'+ writeRandomLetter(abc) +'</td>';
+			for (var j = 1; j <= columns; j++) {
+				temp += '<td data-row="' + i + '" data-column="' + j + '">'+ getRandomElement(abc) +'</td>';
 			};
 			temp += '</tr>';
 		};
 		temp += '</table>'
 		container.append(temp);
+		console.log('Tábla legenerálva');
+	}
+
+	var putFindableWord = function() {
+		var history = [];
+		var findableWord = getRandomElement(words).split('');
+		var activePosition = getStartPoint(dimensions[0], dimensions[1]);
+		for (var i = 0; i < findableWord.length; i++) {
+			var currentCell = container.find('[data-row="' + activePosition[0] + '"][data-column="' + activePosition[1] + '"]');
+			currentCell.text(findableWord[i]);
+			history[i] = activePosition;
+			activePosition = getRandomDirection(activePosition,history);
+			console.log(findableWord[i] + ' letéve: ' + history[i] + ' - következő koordináta: ' + activePosition);
+		}
+		console.log('Keresendő szó elhelyezve');
 	}
 
 	if (container.length != 0) {
-		drawTable(10, 10);
+		console.log('--- Inicializálás ---');
+		drawTable(dimensions[0], dimensions[1]);
 		positionCenterTable();
+		putFindableWord();
 	} else {
+		console.error('Nincs "game" id-val rendelkező elem.');
 		return;
 	}
+
+	
 }
 
 var hunAbc = ['A', 'Á', 'B', 'C', 'D', 'E', 'É', 'F', 'G', 'H', 'I', 'Í', 'J', 'K', 'L', 'M', 'N', 'O', 'Ó', 'Ö', 'Ő', 'P', 'Q', 'R', 'S', 'T', 'U', 'Ú', 'Ü', 'Ű', 'V', 'W', 'X', 'Y', 'Z'];
-var hunWords = ['KAKA', 'PISI', 'VALAMI'];
+var hunWords = ['12345678','MIJAU'];
 
 $(document).ready(function() {
 
